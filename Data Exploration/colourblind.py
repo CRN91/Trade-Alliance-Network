08539332cc943c1aae3765ae4bc30723ddc84a8e
CRN91 <ak824@exeter.ac.uk>
2024-09-30 21:58:05 +0100

    Uploading what was my final submission

---
+++ Data Exploration/colourblind.py
@@ -0,0 +1,26 @@
+""" Colours taken from https://davidmathlogic.com/colorblind/#%231A85FF-%23D41159,
+    and https://personal.sron.nl/~pault/#sec:qualitative """
+
+quantitative = {'grepur': ['#1AFF1A', '#4B0092'], 'yelblu': ['#FFC20A', '#0C7BDC'],
+           'tantuq': ['#E1BE6A', '#40B0A6'], 'yelpnk': ['#FEFE62', '#D35FB7'],
+           'blured': ['#005AB5', '#DC3220'], 'orgmag': ['#E66100', '#5D3A9B'],
+           'brwblu': ['#994F00', '#006CD1'], 'blupnk': ['#1A85FF', '#D41159'],
+           'muted10': ['#CC6677', '#332288', '#DDCC77', '#117733', '#88CCEE',
+                       '#882255', '#44AA99', '#999933', '#AA4499', '#DDDDDD'],
+           'vibrant7': ['#EE7733', '#0077BB', '#33BBEE', '#EE3377', '#CC3311',
+                        '#009988', '#BBBBBB'],
+           'bright7': ['#4477AA', '#EE6677', '#228833', '#CCBB44', '#66CCEE',
+                       '#AA3377', '#BBBBBB'],
+           'high5': ['#004488', '#DDAA33', '#BB5566', '#000000', '#FFFFFF'],
+           'light7': ['#77AADD', '#EE8866', '#EEDD88', '#FFAABB', '#99DDFF',
+                      '#44BB99', '#BBCC33', '#AAAA00', '#DDDDDD']}
+
+divergent = {'sunset': ['#364B9A', '#4A7BB7', '#6EA6CD', '#98CAE1', '#C2E4EF',
+                        '#EAECCC', '#FEDA8B', '#FDB366', '#F67E4B', '#DD3D2D',
+                        '#A50026'],
+             'nightfall': ['#125A56', '#00767B', '#238F9D', '#42A7C6', '#60BCE9',
+                           '#9DCCEF', '#C6DBED', '#DEE6E7', '#ECEADA', '#F0E6B2',
+                           '#F9D576', '#FFB954', '#FD9A44', '#F57634', '#E94C1F',
+                           '#D11807', '#A01813'],
+             'PRGn': ['#762A83', '#9970AB', '#C2A5CF', '#E7D4E8', '#F7F7F7',
+                      '#D9F0D3', '#ACD39E', '#5AAE61', '#1B7837']}
